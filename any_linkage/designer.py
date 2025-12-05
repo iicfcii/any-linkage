b3a2bec3770b0acd5634dd6e5ed1b2ae82f9e55d
Fuchen Chen <8230152+iicfcii@users.noreply.github.com>
2025-12-04 19:59:57 -0700

    Added Design abstract class

---
+++ any_linkage/designer.py
@@ -0,0 +1,141 @@
+import os
+import time
+from abc import ABC, abstractmethod
+import multiprocessing as mp
+import torch
+import torch.optim as optim
+import numpy as np
+from any_linkage.plotter import Plotter
+
+
+class Design(ABC):
+    @staticmethod
+    @abstractmethod
+    def plans():
+        raise NotImplementedError
+
+    def __init__(self, plan_index, seed=0, device="cuda"):
+        self.seed = seed
+        torch.manual_seed(seed)
+        self.device = device
+        self.plan_index = plan_index
+        self.params = []
+        self.plotter_bbox = (-100, -100, 200, 200)
+
+    @abstractmethod
+    def _eval(self):
+        raise NotImplementedError
+
+    def eval(self):
+        self.loss, self.p, self.c = self._eval()
+        return self.loss
+
+    def plot(self):
+        indices = torch.argsort(self.loss)
+        self.q = self.q[indices]
+        for k, v in self.p.items():
+            self.p[k] = v[indices]
+        self.c = [
+            _c[:4] + [_c[4][indices], _c[5][indices]]
+            for _c in self.c
+        ]
+
+        self.plotter = Plotter(
+            self.q, self.p, self.c,
+            self.plotter_bbox,
+            self._on_plotted,
+            self._on_design_changed,
+        )
+
+    def _on_plotted(self, d_index, q_index):
+        pass
+
+    def _on_design_changed(self, d_index, q_index):
+        pass
+
+
+def optimize(
+    design,
+    lr=1, lr_schedule="cosine",
+    n_steps=10000,
+    log_interval=1000,
+    id=None,
+):
+    optimizer = optim.Adam(design.params, lr=lr)
+
+    if os.name != "nt":
+        torch.compiler.reset()
+        eval = torch.compile(design.eval)
+    else:
+        # Not using torch.compile on Windows because it is not well supported.
+        # torch.compile can significantly speed up the optimization.
+        eval = design.eval
+        pass
+
+    t0 = time.time()
+    for i in range(n_steps + 1):
+        loss = eval()
+        loss_mean = torch.mean(loss)
+
+        if i > 0 and i % log_interval == 0:
+            sps = i / (time.time() - t0)
+            loss_min = torch.amin(loss.detach())
+            print(
+                f"id: {id}, "
+                f"iter: {i}, "
+                f"sps: {sps:.2f}, "
+                f"loss_mean: {loss_mean:.2f}, "
+                f"loss_min: {loss_min:.2f}"
+            )
+
+        if i < n_steps:
+            optimizer.zero_grad()
+            loss_mean.backward()
+            optimizer.step()
+            if lr_schedule == "linear":
+                optimizer.param_groups[0]["lr"] = (1 - i / n_steps) * lr
+            elif lr_schedule == "cosine":
+                optimizer.param_groups[0]["lr"] = (
+                    np.cos(i / n_steps * np.pi) + 1
+                ) / 2 * lr
+            else:
+                pass
+
+
+def save(design, path, name="design"):
+    os.makedirs(path, exist_ok=True)
+
+    timestamp = int(time.time())
+    loss_min = torch.amin(design.loss.detach()).item()
+    data = {
+        "timestamp": timestamp,
+        "design": design,
+    }
+
+    name = f"{timestamp}_{name}_{design.plan_index}_{loss_min:.4f}.pt"
+    path = os.path.join(path, name)
+    torch.save(data, path)
+
+
+def load(path):
+    return torch.load(path, weights_only=False)["design"]
+
+
+def _sweep_fun(Design, plan_index, name, optimize_kwargs):
+    design = Design(plan_index)
+    optimize(design, id=plan_index, **optimize_kwargs)
+    save("logs", design, name)
+
+
+def sweep(Design, name="design", processes=1, optimize_kwargs={}):
+    if processes > 1:
+        num_plans = len(Design.plans())
+        args = [
+            (Design, i, name, optimize_kwargs)
+            for i in range(num_plans)
+        ]
+        with mp.Pool(2) as p:
+            p.starmap(_sweep_fun, args, chunksize=1)
+    else:
+        for plan_index in range(len(Design.plans())):
+            _sweep_fun(Design, plan_index, name, optimize_kwargs)
--- examples/design_one_dof_leg.py
+++ examples/design_one_dof_leg.py
@@ -4,51 +4,10 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import any_linkage.topology as topology
 import any_linkage.dimensions as dimensions
-import any_linkage.design as design
-from any_linkage.plotter import Plotter
+import any_linkage.designer as designer
 
 
-class OneDoFLegDesigner():
-    def __init__(self, plan_index, seed=0, device="cuda"):
-        self.plan_index = plan_index
-        self.seed = seed
-        torch.manual_seed(seed)
-        self.device = device
-
-        self.plan = OneDoFLegDesigner.plans()[self.plan_index]
-        self.c_empty = dimensions.gen_constraints(self.plan)
-        self.output_key = dimensions.get_output_key(self.c_empty)
-
-        self.n_designs = 1000
-        self.n_q_combs = 10
-
-        self.cos_max = torch.tensor(0.8).to(self.device)
-
-        self.weights = torch.tensor([1, 1000]).to(self.device)
-
-        self.params = []
-        self.p0 = {}
-        for key in dimensions.get_point_keys(self.c_empty):
-            if key == dimensions.origin_key:
-                p = torch.zeros([self.n_designs, 2]).to(self.device)
-            else:
-                p = torch.zeros([self.n_designs, 2]).to(self.device)
-                p[:, 0].uniform_(-100, 100)
-                p[:, 1].uniform_(-100, 100)
-                p.requires_grad_(True)
-                self.params.append(p)
-            self.p0[key] = p
-
-        self.q = torch.linspace(
-            -1, 1, self.n_q_combs,
-        ).unsqueeze(-1).expand(self.n_designs, -1, -1).to(self.device)
-
-        self.p_output_d = torch.zeros([self.n_q_combs, 2]).to(self.device)
-        self.p_output_d[:, 1] = torch.linspace(
-            -100, 0, self.n_q_combs,
-        ).to(self.device)
-
-    @staticmethod
+class OneDoFLegDesign(designer.Design):
     def plans():
         plans = topology.load()
 
@@ -72,15 +31,51 @@ class OneDoFLegDesigner():
                 filtered_plans.append(plan)
         return filtered_plans
 
-    def eval(self):
-        self.c = dimensions.populate(self.p0, self.c_empty)
-        self.p, cos_theta, cos_theta_p, cos_mu = dimensions.fk(
-            self.q, self.p0, self.c,
+    def __init__(self, plan_index, seed=0, device="cuda"):
+        super().__init__(plan_index, seed=seed, device=device)
+        self.plotter_bbox = (-200, -200, 400, 400)
+
+        self.plan = OneDoFLegDesign.plans()[self.plan_index]
+        self.c_empty = dimensions.gen_constraints(self.plan)
+        self.output_key = dimensions.get_output_key(self.c_empty)
+
+        self.n_designs = 1000
+        self.n_q_combs = 10
+
+        self.cos_max = torch.tensor(0.8).to(self.device)
+
+        self.weights = torch.tensor([1, 1000]).to(self.device)
+
+        self.p0 = {}
+        for key in dimensions.get_point_keys(self.c_empty):
+            if key == dimensions.origin_key:
+                p = torch.zeros([self.n_designs, 2]).to(self.device)
+            else:
+                p = torch.zeros([self.n_designs, 2]).to(self.device)
+                p[:, 0].uniform_(-100, 100)
+                p[:, 1].uniform_(-100, 100)
+                p.requires_grad_(True)
+                self.params.append(p)
+            self.p0[key] = p
+
+        self.q = torch.linspace(
+            -1, 1, self.n_q_combs,
+        ).unsqueeze(-1).expand(self.n_designs, -1, -1).to(self.device)
+
+        self.p_output_d = torch.zeros([self.n_q_combs, 2]).to(self.device)
+        self.p_output_d[:, 1] = torch.linspace(
+            -100, 0, self.n_q_combs,
+        ).to(self.device)
+
+    def _eval(self):
+        c = dimensions.populate(self.p0, self.c_empty)
+        p, cos_theta, cos_theta_p, cos_mu = dimensions.fk(
+            self.q, self.p0, c,
         )
 
         loss_output_error = torch.mean(
             torch.linalg.norm(
-                self.p[self.output_key] - self.p_output_d,
+                p[self.output_key] - self.p_output_d,
                 dim=-1,
             ), dim=-1,
         )
@@ -89,47 +84,28 @@ class OneDoFLegDesigner():
         cos_max = torch.amax(torch.abs(cos), dim=(1, 2))
         loss_cos_max = torch.maximum(cos_max, self.cos_max) - self.cos_max
 
-        self.loss_itemized = torch.stack(
+        loss_itemized = torch.stack(
             [
                 loss_output_error,
                 loss_cos_max,
             ],
             dim=1,
         )
-        self.loss_weighted = self.weights * self.loss_itemized
-        self.loss_weighted_sum = torch.sum(
-            self.weights * self.loss_itemized, dim=1,
-        )
+        self.loss_weighted = self.weights * loss_itemized
+        loss = torch.sum(self.weights * loss_itemized, dim=1)
 
-        return self.loss_weighted_sum
+        return loss, p, c
 
-    def plot(self):
-        indices = torch.argsort(self.loss_weighted_sum)
-        self.q = self.q[indices]
-        for k, v in self.p.items():
-            self.p[k] = v[indices]
-        self.c = [
-            _c[:4] + [_c[4][indices], _c[5][indices]]
-            for _c in self.c
-        ]
-
-        self.plotter = Plotter(
-            self.q, self.p, self.c,
-            (-200, -200, 400, 400),
-            self.on_plotted,
-            self.on_design_changed,
-        )
-
-    def on_plotted(self, d_index, q_index):
+    def _on_plotted(self, d_index, q_index):
         p_output = self.p[self.output_key][d_index].detach().cpu().numpy()
         p_output_d = self.p_output_d.detach().cpu().numpy()
         plt.plot(p_output[:, 0], p_output[:, 1], 'b', lw=1)
         plt.plot(p_output_d[:, 0], p_output_d[:, 1], 'g', lw=1)
 
-    def on_design_changed(self, d_index, q_index):
+    def _on_design_changed(self, d_index, q_index):
         print(
             f"design: {d_index}, "
-            f"l: {self.loss_weighted_sum[d_index]:.4f}, "
+            f"l: {self.loss[d_index]:.4f}, "
             f"l_oe: {self.loss_weighted[d_index][0]:.4f}, "
             f"l_cos: {self.loss_weighted[d_index][1]:.4f}"
         )
@@ -138,24 +114,24 @@ class OneDoFLegDesigner():
 def main():
     if sys.argv[1] == "t":
         plan_index = int(sys.argv[2])
-        designer = OneDoFLegDesigner(plan_index)
-        designer.eval()
-        designer.plot()
+        design = OneDoFLegDesign(plan_index)
+        design.eval()
+        design.plot()
         plt.show()
 
     if sys.argv[1] == "o":
         plan_index = int(sys.argv[2])
-        designer = OneDoFLegDesigner(plan_index)
-        design.optimize(designer.params, designer.eval, id=plan_index)
-        design.save("logs", designer, name="one_dof_leg")
+        design = OneDoFLegDesign(plan_index)
+        designer.optimize(design, id=plan_index)
+        designer.save(design, "logs", name="one_dof_leg")
 
     if sys.argv[1] == "s":
-        design.sweep(OneDoFLegDesigner, name="one_dof_leg", processes=2)
+        designer.sweep(OneDoFLegDesign, name="one_dof_leg", processes=2)
 
     if sys.argv[1] == "p":
         path = sys.argv[2]
-        designer = design.load(path)
-        designer.plot()
+        design = designer.load(path)
+        design.plot()
         plt.show()
 
 
